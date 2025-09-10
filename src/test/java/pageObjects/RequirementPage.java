39a928d275af66a54d78d29e7ef04e4c42bf857d
MayukhjitChakraborty <mayukhjit_c@pursuitsoftware.com>
2025-09-10 10:13:56 +0530

    RequirementPage added

---
+++ src/test/java/pageObjects/RequirementPage.java
@@ -0,0 +1,14 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class RequirementPage extends BasePage {
+    public RequirementPage(WebDriver driver){
+        super(driver);
+    }
+
+    //Locators
+
+    //Actions
+
+}
