60247e7b355136b7b46430061b9dcbb20ea4a218
MayukhjitChakraborty <mayukhjit_c@pursuitsoftware.com>
2025-09-17 13:02:23 +0530

    updated

--- src/test/java/pageObjects/authoTestCaseTab/AuthorTestCasePage.java
+++ src/test/java/pageObjects/authoTestCaseTab/AuthorTestCasePage.java
@@ -1,6 +1,5 @@
 package pageObjects.authoTestCaseTab;
 
-import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,12 +8,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.BasePage;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class AuthorTestCasePage extends BasePage {
-    public AuthorTestCasePage(WebDriver driver){
+    public AuthorTestCasePage(WebDriver driver) {
         super(driver);
     }
     // locators
@@ -34,7 +31,7 @@ public class AuthorTestCasePage extends BasePage {
     WebElement dropdownFeature;
 
     public WebElement linkRequirement(String reqId) {
-        return driver.findElement(By.xpath("//div[text()='"+ reqId +"']"));
+        return driver.findElement(By.xpath("//div[text()='" + reqId + "']"));
     }
 
     @FindBy(xpath = "//p[@class='supporting-text']")
@@ -67,19 +64,58 @@ public class AuthorTestCasePage extends BasePage {
     @FindBy(xpath = "//h3[text()='Create Test Cases']")
     WebElement headingCreateTestCases;
 
+
+    public WebElement actionIconForTestcase(String testCaseId) {
+        return driver.findElement(By.xpath("//a[text()='" + testCaseId + "']/ancestor::div[@class='testlistrow']//div[@class='testlistcell48']//img"));
+    }
+
+    @FindBy(id = "confirmBtn")
+    WebElement buttonYes;
+
+    @FindBy(id = "cancelBtn")
+    WebElement buttonNo;
+
+    @FindBy(xpath = "//img[@alt='Close Sidebar']")
+    WebElement buttonCollapseToggle;
+
+    @FindBy(xpath = "//img[@alt='Open Sidebar']")
+    WebElement buttonExpandToggle;
+
+    //This will fetch Locator for any linked Test case inside
+    public WebElement linkTestCaseIdFromName(String name){
+        return driver.findElement(By.xpath("//p[text()='"+name+"']/ancestor::div[@class='testlistrow']//a"));
+    }
+
+    public WebElement linkTestCaseIdFromId(String id){
+        return driver.findElement(By.xpath("//div[@class='testlistcell']/a[text()='"+id+"']"));
+    }
+
+    @FindBy(xpath = "//div[normalize-space()='LINK TESTCASE']")
+    WebElement LinkTestcase;
+
+
+    @FindBy(xpath = "//input[@id='searchInputTCModal']")
+    WebElement inputSearchTestCase;
+
+    @FindBy(xpath = "//button[@id='searchTCButton']")
+    WebElement buttonSearch;
+
+    @FindBy(xpath = "//div[@class='defect-modal-text-wrapper-3']")
+    WebElement Pid;
+
     @FindBy(xpath = "//div[@class='testlistcell']/a")
     List<WebElement> linkAllTestCaseId;
 
 
     //actions
 
-    public void selectEpic(String epicName){
-        Select s= new Select(dropdownEpic);
+    public void selectEpic(String epicName) {
+        Select s = new Select(dropdownEpic);
         s.selectByVisibleText(epicName);
     }
 
-    public void selectFeature(String featureName){
-        Select s= new Select(dropdownFeature);
+    public void selectFeature(String featureName) {
+        Select s = new Select(dropdownFeature);
         s.selectByVisibleText(featureName);
     }
 
@@ -89,60 +125,74 @@ public class AuthorTestCasePage extends BasePage {
         Thread.sleep(2000);
     }
 
-    public String showRequirementHeader(){
+    public String showRequirementHeader() {
         return headingRequirement.getText();
     }
 
-    public void clickAddTestcase(){
+    public void clickAddTestcase() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", buttonAddTestCase);
     }
-    public void clickAuthorTestcase(){
+
+    public void clickAuthorTestcase() {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", tabAuthorTestcase);
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", tabAuthorTestcase);
+    }
+    public void clickActionIcon(String testCaseId) throws InterruptedException {
+        Thread.sleep(2000);
+        actionIconForTestcase(testCaseId).click();
+        Thread.sleep(2000);
     }
 
 
     public String getEpicLabelName(){
         return labelEpic.getText();
     }
-    public boolean getVisibilityOfEpic(){
+
+    public boolean getVisibilityOfEpic() {
         return dropdownEpic.isDisplayed();
     }
+
     public int getCountInEpic() throws InterruptedException {
         Thread.sleep(2000);
         return optionsEpic.size();
     }
+
     public List<WebElement> getAllEpics() throws InterruptedException {
         Thread.sleep(2000);
         return optionsEpic;
     }
+
     public void getAllFeatures() throws InterruptedException {
         for (WebElement feature : optionsFeatures) {
             System.out.println("Feature: " + feature.getText());
         }
     }
-    public void clickEpic(){
+
+    public void clickEpic() {
         dropdownEpic.click();
     }
-    public void clickFeature(){
+
+    public void clickFeature() {
         dropdownFeature.click();
     }
-    public String getSelectedEpic(){
+
+    public String getSelectedEpic() {
         Select s = new Select(dropdownEpic);
         return s.getFirstSelectedOption().getText();
     }
 
-    public String getSelectedFeature(){
+    public String getSelectedFeature() {
         Select s = new Select(dropdownFeature);
         return s.getFirstSelectedOption().getText();
     }
+
     public int getCountRQInFeature() throws InterruptedException {
         Thread.sleep(2000);
         return rqCountWrapper.size();
     }
 
-    public void clickLastPageArrowBtn(){
+    public void clickLastPageArrowBtn() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(lastPageArrowBtn));
         lastPageArrowBtn.click();
@@ -194,9 +244,10 @@ public class AuthorTestCasePage extends BasePage {
         }
     }
 
-    public boolean getFeatureVisibility(){
+    public boolean getFeatureVisibility() {
         return dropdownFeature.isDisplayed();
     }
+
     public String showPaginationOfRequirement() throws InterruptedException {
         Thread.sleep(2000);
         return divRequirementPagination.getText();
@@ -207,8 +258,19 @@ public class AuthorTestCasePage extends BasePage {
         Thread.sleep(2000);
     }
 
+
     public void clickPreviousArrow(){
         arrowBackwardPrevious.click();
+    }
+
+    public boolean isAddTestCaseButtonVisible1() {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement element = wait.until(ExpectedConditions.visibilityOf(buttonAddTestCase));
+            return element.isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
     }
 
     public boolean isCreateTextHeadingVisible() {
@@ -219,6 +281,57 @@ public class AuthorTestCasePage extends BasePage {
         } catch (TimeoutException e) {
             return false; // element not visible within wait time
         }
+    }
+
+
+    public void clickCollapseToggle() throws InterruptedException {
+        Thread.sleep(1000);
+        buttonCollapseToggle.click();
+        Thread.sleep(2000);
+    }
+
+    public void clickExpandToggle() throws InterruptedException {
+        Thread.sleep(1000);
+        buttonExpandToggle.click();
+        Thread.sleep(2000);
+    }
+
+
+    public void clicklinktestcase() throws InterruptedException {
+        Thread.sleep(3000);
+        LinkTestcase.click();
+    }
+
+    public void confirmUnlink() throws InterruptedException {
+        Thread.sleep(1000);
+        buttonYes.click();
+        Thread.sleep(2000);
+    }
+
+    public void cancelUnlink() throws InterruptedException {
+        Thread.sleep(1000);
+        buttonNo.click();
+        Thread.sleep(2000);
+    }
+
+
+    public boolean isRowDeleted(String testcaseId)
+    {
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            boolean isInvisible = wait.until((ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='testlistcell']/a[text()='"+testcaseId+"']"))));
+            return isInvisible;
+        }
+        catch (Exception e)
+        {
+            return false ;
+        }
+    }
+    public void clickTestCase(String tcID){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions
+                .elementToBeClickable(linkTestCaseIdFromId(tcID)));
+        element.click();
     }
 
     public boolean isAllTestIdSorted() throws InterruptedException {
@@ -239,13 +352,5 @@ public class AuthorTestCasePage extends BasePage {
         return false;
     }
 
-    public boolean isAddTestCaseButtonVisible1() {
-        try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement element = wait.until(ExpectedConditions.visibilityOf(buttonAddTestCase));
-            return element.isDisplayed();
-        } catch (TimeoutException e) {
-            return false;
-        }
-    }
 }
+
