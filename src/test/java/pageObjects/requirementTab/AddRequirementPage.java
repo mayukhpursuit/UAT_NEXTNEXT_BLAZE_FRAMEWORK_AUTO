1ec8e17af3cce6235cfe3d637e130c4dbfc96e1f
VivekRajput <vivekrajput9575@gmail.com>
2025-09-23 15:14:57 +0530

    Complete the Addrequirement Scanning

--- src/test/java/pageObjects/requirementTab/AddRequirementPage.java
+++ src/test/java/pageObjects/requirementTab/AddRequirementPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.BasePage;
 
@@ -27,6 +28,24 @@ public class AddRequirementPage extends BasePage {
     @FindBy(xpath = "//button[@id='saveButton']")
     WebElement buttonSave;
 
+    @FindBy(xpath = "//button[@id='createRequirementButton']")
+    WebElement addRequirementBtn;
+
+    @FindBy(xpath = "//select[option[contains(text(), 'Select Priority')]]")
+    WebElement dropDownPriority;
+
+    @FindBy(xpath = "//select[option[contains(text(), 'Select Status')]]")
+    WebElement dropDownStatus;
+
+    @FindBy(xpath = "//select[option[contains(text(), 'Select Type')]]")
+    WebElement dropDownType;
+
+    @FindBy(xpath = "//button[@id='closeButton']")
+    WebElement buttonClose;
+
+    @FindBy(xpath = "//img[@id='rotatable-image']")
+    WebElement requirementExpandCollapseArrow;
+
     //actions
 
     public void setRequirementId(String id){
@@ -45,8 +64,43 @@ public class AddRequirementPage extends BasePage {
         // Send description
         editableField.sendKeys(description);
     }
+    public void clickAddRequirementBtn(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        addRequirementBtn.click();
+    }
+    public void selectPriority(String priority){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(dropDownPriority));
+        Select s= new Select(dropDownPriority);
+        s.selectByVisibleText(priority);
+    }
+    public void selectStatus(String status){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(dropDownStatus));
+        Select s= new Select(dropDownStatus);
+        s.selectByVisibleText(status);
+    }
+    public void selectType(String type){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(dropDownType));
+        Select s= new Select(dropDownType);
+        s.selectByVisibleText(type);
+    }
+
     public void clickSave(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonSave));
         buttonSave.click();
+    }
+    public void clickClose(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonClose));
+        buttonClose.click();
+    }
+    public void requirementDetailsVisibility(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(requirementExpandCollapseArrow));
+        requirementExpandCollapseArrow.click();
     }
 
 
