946b6741703087e35c7f5bf99cf3cb518c7d8cca
SohailShaikhFaiyaz <sohail_sf@pursuitsoftware.com>
2025-09-23 12:02:54 +0530

    Resolved Commit

--- src/test/java/testCases/testPlanTabTestCase/TC001.java
+++ src/test/java/testCases/testPlanTabTestCase/TC001.java
@@ -1,26 +1,50 @@
 package testCases.testPlanTabTestCase;
 
-import DataProviders.RequirementDataProvider;
-import DataProviders.TestPlanDataProvider;
 import org.testng.annotations.Test;
+import pageObjects.testPlanTab.TestPlanPage;
 import testBase.BaseClass;
 
 public class TC001 extends BaseClass {
-    @Test(dataProvider = "tc001", dataProviderClass = TestPlanDataProvider.class)
-    public void verifyEpicVisibility(
-    ) throws InterruptedException {
-        logger.info("****** Starting the Test Case *****************");
+
+    @Test
+    public void verifyTestPlanMenuButtons() throws InterruptedException {
+        logger.info("****** Starting the Test Case: Verify TestPlan Menu Buttons *****************");
         try {
+
             login();
             logger.info("Logged in successfully");
 
-        }
-        catch (AssertionError e) {
-            logger.error("Assertion failed: " + e.getMessage());
-            throw e;
-        }
-        catch (Exception e) {
-            logger.error("Exception occurred: " + e.getMessage());
+            TestPlanPage testPlanPage = new TestPlanPage(getDriver());
+
+            testPlanPage.selectTestPlanTab();
+            logger.info("Selected Test Plan tab");
+
+            testPlanPage.expandSidebarIfCollapsed();
+            logger.info("Expanded sidebar if it was collapsed");
+
+            testPlanPage.expandProjectSTG();
+            logger.info("Expanded project STG- PulseCodeOnAzureCloud");
+
+            testPlanPage.expandRelease("New Release 3-9-2025");
+            logger.info("Expanded Release - New Release 3-9-2025");
+
+            testPlanPage.clickNewRelease();
+            logger.info("Clicked on New Release button");
+
+            testPlanPage.clickNewTestCycle();
+            logger.info("Clicked on New TestCycle button");
+
+            testPlanPage.clickNewTestSuite();
+            logger.info("Clicked on New TestSuite button");
+
+            testPlanPage.clickDelete();
+            logger.info("Clicked on Delete button");
+
+            testPlanPage.toggleHamburgerMenu();
+            logger.info("Toggled Hamburger Menu");
+
+        } catch (Exception e) {
+            logger.error("Exception occurred: " + e.getMessage(), e);
             throw e;
         }
         logger.info("************ Test Case Finished *************************");
