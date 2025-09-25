17c146fa88cf0d382c20c4a60468af3b6328a857
MohitAman <mohit_a@pursuitsoftware.com>
2025-09-25 11:12:17 +0530

    tc013/14 added

---
+++ src/test/java/testCases/requirementTabTestCase/TC013.java
@@ -0,0 +1,37 @@
+package testCases.requirementTabTestCase;
+
+import DataProviders.RequirementDataProvider;
+import org.testng.annotations.Test;
+import pageObjects.requirementTab.AddRequirementPage;
+import pageObjects.requirementTab.IndividualModulePage;
+import pageObjects.requirementTab.RequirementTabPage;
+import testBase.BaseClass;
+
+public class TC013 extends BaseClass {
+    @Test
+    public void verifyuserisabletoclickonhelp () throws InterruptedException {
+        logger.info("****** Starting the TC:13 verify user is able to click on help   *************");
+        try {
+            login();
+            logger.info("Logged in successfully");
+
+            RequirementTabPage reqPage = new RequirementTabPage(getDriver());
+            reqPage.clickRequirementTab();
+            logger.info("clicked on requirement tab");
+
+            reqPage.verifyHelpDropdown(getDriver());
+            logger.info("Help dropdown is visible");
+
+
+        }
+        catch (AssertionError e) {
+            logger.error("Assertion failed: " + e.getMessage());
+            throw e;
+        }
+        catch (Exception e) {
+            logger.error("Exception occurred: " + e.getMessage());
+            throw e;
+        }
+        logger.info("************ TC:13 Finished *************************");
+    }
+}
