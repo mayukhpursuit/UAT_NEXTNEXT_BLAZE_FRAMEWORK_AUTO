3cf04ba965fe05e2b148213c65ee6d0ce9437e17
SohailShaikhFaiyaz <sohail_sf@pursuitsoftware.com>
2025-10-07 12:28:28 +0530

    Check for Empty release name - release name can be empty

--- src/test/java/pageObjects/testPlanTab/TestPlanLandingPage.java
+++ src/test/java/pageObjects/testPlanTab/TestPlanLandingPage.java
@@ -395,9 +395,8 @@ public class TestPlanLandingPage extends BasePage {
         List<String> expectedProjects = new ArrayList<>();
         for (WebElement el : projectElements) {
             String text = el.getText().trim();
-            if (!text.isEmpty()) {
-                expectedProjects.add(text);
-            }
+            expectedProjects.add(text);
+
         }
         return expectedProjects;
     }
