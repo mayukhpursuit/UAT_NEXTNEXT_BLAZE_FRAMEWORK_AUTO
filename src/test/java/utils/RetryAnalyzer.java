db2576e2b276d1c23f07559f4681df6d9e1e8e87
MayukhjitChakraborty <mayukhjit_c@pursuitsoftware.com>
2025-10-10 23:09:50 +0530

    Release 1.0 10/10/2025

--- src/test/java/utils/RetryAnalyzer.java
+++ src/test/java/utils/RetryAnalyzer.java
@@ -7,6 +7,7 @@ public class RetryAnalyzer implements IRetryAnalyzer {
     private int count = 0;
     private static final int MAX_RETRY = 4;  // run up to 3 times
 
+
     @Override
     public boolean retry(ITestResult result) {
         if (count < MAX_RETRY) {
