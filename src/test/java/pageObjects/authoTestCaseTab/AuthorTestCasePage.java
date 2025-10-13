744016d2c2440984a6c24f280b9b2f09b36d8559
MayukhjitChakraborty <mayukhjit_c@pursuitsoftware.com>
2025-10-13 11:26:11 +0530

    Test 4

--- src/test/java/pageObjects/authoTestCaseTab/AuthorTestCasePage.java
+++ src/test/java/pageObjects/authoTestCaseTab/AuthorTestCasePage.java
@@ -284,6 +284,8 @@ public class AuthorTestCasePage extends BasePage {
 
     public void clickNextArrow() throws InterruptedException {
         Thread.sleep(2000);
+        divRequirementPagination.click();
+        new Actions(driver).moveToElement(arrowForwardNextPagination);
         arrowForwardNextPagination.click();
         Thread.sleep(3000);
     }
