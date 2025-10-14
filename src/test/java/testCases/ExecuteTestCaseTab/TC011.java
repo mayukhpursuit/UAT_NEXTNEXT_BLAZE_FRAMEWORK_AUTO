584f3ae46008dbc1a29993885b726aac69ee6fcb
MohitAman <mohit_a@pursuitsoftware.com>
2025-10-14 16:52:15 +0530

    added tc011

---
+++ src/test/java/testCases/ExecuteTestCaseTab/TC011.java
@@ -0,0 +1,58 @@
+package testCases.ExecuteTestCaseTab;
+
+import DataProviders.ExecuteTestCaseDataProvider;
+import org.testng.annotations.Test;
+import pageObjects.executeTestCaseTab.ExecuteLandingPage;
+import testBase.BaseClass;
+import utils.RetryAnalyzer;
+
+public class TC011 extends BaseClass {
+
+    @Test(dataProvider = "tc011", dataProviderClass = ExecuteTestCaseDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
+    public void verifyusercanselectatestcasebyclickingontestrunID(
+            String projectName,
+            String ReleaseName,
+            String CycleName,
+            String SuiteName,
+            String SearchTR
+    ) throws InterruptedException {
+
+        logger.info("****** Starting Test Case: verify user can select a testcase by clicking on test run ID *****************");
+
+        try {
+            login();
+            logger.info("Logged in successfully");
+
+            ExecuteLandingPage executeLandingPage = new ExecuteLandingPage(getDriver());
+            executeLandingPage.clickExecuteTab();
+            logger.info("Clicked on the Execute Test Case tab");
+
+            executeLandingPage.clickArrowRightPointingForExpandModule(projectName);
+            logger.info("Expanded Project: " + projectName);
+
+            executeLandingPage.expandRelease(ReleaseName);
+            logger.info("Expanded Release: " + ReleaseName);
+
+            executeLandingPage.expandSubTestCycle(CycleName);
+            logger.info("Expanded Cycle: " + CycleName);
+
+            executeLandingPage.clickOnSuite(SuiteName);
+            logger.info("Clicked on Suite: " + SuiteName);
+
+            executeLandingPage.clickOnTestRunById(SearchTR);
+            logger.info("Clicked on Test Run ID: " + SearchTR);
+
+
+
+        } catch (AssertionError e) {
+            logger.error("Assertion failed: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("Exception occurred: {}", e.getMessage());
+            throw e;
+        }
+
+        logger.info("************ Test Case Finished *************************");
+    }
+}
+
