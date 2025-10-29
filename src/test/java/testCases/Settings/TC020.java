5dbb7190b2b3d4f95435f8a66abfb05575599acf
VivekRajput <vivekrajput9575@gmail.com>
2025-10-29 17:58:08 +0530

    TC020,TC021 Added

---
+++ src/test/java/testCases/Settings/TC020.java
@@ -0,0 +1,40 @@
+package testCases.Settings;
+
+import org.testng.annotations.Test;
+import pageObjects.Settings.GlobalTabPage;
+import pageObjects.Settings.OtherTabPage;
+import testBase.BaseClass;
+
+public class TC020 extends BaseClass {
+    @Test
+    public void Verify_that_user_is_able_to_select_Requirements_tab(
+
+    ) throws InterruptedException {
+
+        logger.info("****** Starting the TC009: Verify that user is able to create a Custom Field inside Module Tab *****************");
+
+        try {
+            login();
+            logger.info("Logged in successfully");
+
+            GlobalTabPage globalTab = new GlobalTabPage(getDriver());
+            OtherTabPage otherTab = new OtherTabPage(getDriver());
+
+            globalTab.clickCurrentUserAndGoToSettings();
+            logger.info("Clicked on Settings option from user dropdown");
+
+            otherTab.clickRequirement();
+            logger.info("Navigated to Requirements Tab");
+
+
+        } catch (AssertionError e) {
+            logger.error("Assertion failed: " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            logger.error("Exception occurred: " + e.getMessage());
+            throw e;
+        }
+
+        logger.info("************ Test Case Finished *************************");
+    }
+}
