028b98e63e511fba41e363bf9c5d3493c3cb8bd4
ArkajitTah <arkajit_t@pursuitsoftware.com>
2025-11-04 15:25:53 +0530

    Module PageObject added

--- src/test/java/testCases/Settings/TC040.java
+++ src/test/java/testCases/Settings/TC040.java
@@ -8,10 +8,10 @@ import testBase.BaseClass;
 import utils.RetryAnalyzer;
 
 public class TC040 extends BaseClass {
-    @Test(dataProvider = "tc040", dataProviderClass = SettingTestCaseDataProvider.class, retryAnalyzer = RetryAnalyzer.class)
+    @Test(dataProvider = "tc040", dataProviderClass = SettingTestCaseDataProvider.class)
     public void Verify_that_user_is_able_to_add_a_Default_Field_Value_in_the_Release_Tab(
-            String fieldName,
-            String value
+            String fieldName1, String fieldType, String fieldName2,
+            String value, String fieldName3
     ) throws InterruptedException {
 
         logger.info("****** Starting the TC016: Verify that user is able to add a Default Field Value in the Release Tab. *****************");
@@ -29,9 +29,21 @@ public class TC040 extends BaseClass {
             otherTab.clickTestCycle();
             logger.info("Navigated to release tab");
 
+            otherTab.clickOnAddCustomField();
+            logger.info("Clicked on Add Custom Field button");
 
-            otherTab.clickOnEdit(fieldName);
-            logger.info("clicked on edit row:"+fieldName);
+            otherTab.createCustomEnterFieldName(fieldName1);
+            logger.info("Entered Custom Field Name: " + fieldName1);
+
+            otherTab.createCustomSelectDataType(fieldType);
+            logger.info("Selected Custom Field Type: " + fieldType);
+
+            otherTab.clickcreatefieldButton();
+            logger.info("clicked on create field button#");
+
+
+            otherTab.clickOnEdit(fieldName2);
+            logger.info("clicked on edit row:"+fieldName2);
 
             otherTab.clickDefaultAddValue() ;
             logger.info("clicked on adddefaultvalue");
@@ -41,6 +53,12 @@ public class TC040 extends BaseClass {
 
             otherTab.clickDefaultSaveChanges();
             logger.info("clicked on save changes");
+
+            otherTab.clickOnDelete(fieldName3);
+            logger.info("Clicked Delete icon for the field: " + fieldName3);
+
+            otherTab.clickOnDeleteRowConfirmation();
+            logger.info("clicked on yes confirmation");
 
 
         } catch (AssertionError e) {
